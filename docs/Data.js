021d3d02340c26bee950b18868b6ceb4a47b0c10
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-01-08 16:18:10 -0600

    Add files via upload
    overhaul to v3 , multipage java script shopping cart function..... protype ran off src folder containing all of these files. hopping to migrate and acomplish the same thru doc folder path and editing the links in the protype html files to match this new path..... wish me luck

---
+++ docs/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+	{
+	id: "wefumuqsd",
+	name: "mode_ZEN",
+	price: "4.75",
+	desc: "sanctuary in the solidtude of self",
+	img: "assets/mode_ZEN.jpg"},
+	
+	{
+	id: "sdfaggrtv",
+	name: "flower_SUN",
+	price: "4.75",
+	desc: "I'm gonna soak up the sun I'm gonna tell everyone",
+	img: "assets/starFLOWER.jpg"},
+		
+	{
+	id: "adsfads",
+	name: "plumerian_EQUILIBRIUM",
+	price: "4.75",
+	desc: "BEAUTIFLLY balanced.. or imbalanced..",
+	img: "assets/plumerian_EQUILIBRIUM.jpg"},	
+
+	{
+	id: "sgkmjnvads",
+	name: "slice_ORANGE",
+	price: "4.75",     
+	desc: "taste the citrus",
+	img: "assets/slice_ORANGE.jpg"}];
+
+/*let basket = JSON.parse(localStorage.getItem("data")) || [];*/
