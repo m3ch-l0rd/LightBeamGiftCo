a3dd7bdde275241c5b17f54c3542fce4401359a9
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-01-09 13:17:24 -0600

    Update Data.js
    product data updates

--- docs/Data.js
+++ docs/Data.js
@@ -1,61 +1,62 @@
 let shopItemsData = [
 	{
-	id: "000-000",
+	id: "ooa",
 	name: "mode_ZEN",
 	price: "4.75",
 	desc: "sanctuary in the solidtude of self",
 	img: "docs/assets/images/mode_ZEN.jpg"},
 	
 	{
-	id: "000-001",
+	id: "oob",
 	name: "flower_SUN",
 	price: "4.75",
 	desc: "I'm gonna soak up the sun I'm gonna tell everyone",
 	img: "docs/assets/images/starFLOWER.jpg"},
 		
 	{
-	id: "000-002",
+	id: "ooc",
 	name: "plumerian_EQUILIBRIUM",
 	price: "4.75",
 	desc: "BEAUTIFLLY balanced.. or imbalanced..",
 	img: "docs/assets/images/plumerian_EQUILIBRIUM.jpg"},	
 
 	{
-	id: "000-003",
+	id: "ood",
 	name: "slice_ORANGE",
 	price: "4.75",     
 	desc: "taste the citrus",
 	img: "docs/assets/images/slice_ORANGE.jpg"},
 	
 	{
-	id: "000-004",
+	id: "ooe",
 	name: "bird_LOVE",
 	price: "4.75",
 	desc: "sanctuary in the solidtude of self",
 	img: "docs/assets/images/bird_LOVE.jpg"},
 	
 	{
-	id: "000-005",
+	id: "oof",
 	name: "bud_ROSE",
 	price: "4.75",
 	desc: "I like to think about big phat roses...",
 	img: "docs/assets/images/bud_ROSE.jpg"},
 		
 	{
-	id: "000-006",
-	name: "plumerian_EQUILIBRIUM",
+	id: "oog",
+	name: "drip_VINE",
 	price: "4.75",
-	desc: "xxxxxxxxx",
-	img: "docs/assets/images/.jpg"},	
+	desc: "green and wet",
+	img: "docs/assets/images/drip_VINE.jpg"},	
 
 	{
-	id: "000-007",
-	name: "",
+	id: "ooh",
+	name: "cosmos_FLOWER",
 	price: "4.75",     
-	desc: "zzzzzzz",
-	img: "docs/assets/images/.jpg"}];
+	desc: "wish upon a flower",
+	img: "docs/assets/images/cosmos_FLOWER.jpg"}];
 
 /*let basket = JSON.parse(localStorage.getItem("data")) || [];*/
+
 
 
 
