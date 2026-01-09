3f0160d7c0067168aeca957512c5ed7f43a2faf7
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-01-08 23:47:28 -0600

    Update Data.js
    update product data 
    item id system, and new product id imgs

--- docs/Data.js
+++ docs/Data.js
@@ -1,61 +1,62 @@
 let shopItemsData = [
 	{
-	id: "wefumuqsd",
+	id: "000-000",
 	name: "mode_ZEN",
 	price: "4.75",
 	desc: "sanctuary in the solidtude of self",
 	img: "docs/assets/images/mode_ZEN.jpg"},
 	
 	{
-	id: "sdfaggrtv",
+	id: "000-001",
 	name: "flower_SUN",
 	price: "4.75",
 	desc: "I'm gonna soak up the sun I'm gonna tell everyone",
 	img: "docs/assets/images/starFLOWER.jpg"},
 		
 	{
-	id: "adsfads",
+	id: "000-002",
 	name: "plumerian_EQUILIBRIUM",
 	price: "4.75",
 	desc: "BEAUTIFLLY balanced.. or imbalanced..",
 	img: "docs/assets/images/plumerian_EQUILIBRIUM.jpg"},	
 
 	{
-	id: "sgkmjnvads",
+	id: "000-003",
 	name: "slice_ORANGE",
 	price: "4.75",     
 	desc: "taste the citrus",
 	img: "docs/assets/images/slice_ORANGE.jpg"},
 	
 	{
-	id: "wefumuqsd",
-	name: "mode_ZEN",
+	id: "000-004",
+	name: "bird_LOVE",
 	price: "4.75",
 	desc: "sanctuary in the solidtude of self",
 	img: "docs/assets/images/bird_LOVE.jpg"},
 	
 	{
-	id: "sdfaggrtv",
-	name: "flower_SUN",
+	id: "000-005",
+	name: "bud_ROSE",
 	price: "4.75",
-	desc: "I'm gonna soak up the sun I'm gonna tell everyone",
+	desc: "I like to think about big phat roses...",
 	img: "docs/assets/images/bud_ROSE.jpg"},
 		
 	{
-	id: "adsfads",
+	id: "000-006",
 	name: "plumerian_EQUILIBRIUM",
 	price: "4.75",
-	desc: "BEAUTIFLLY balanced.. or imbalanced..",
-	img: "docs/assets/images/slice_ORANGE.jpg"},	
+	desc: "xxxxxxxxx",
+	img: "docs/assets/images/.jpg"},	
 
 	{
-	id: "sgkmjnvads",
-	name: "slice_ORANGE",
+	id: "000-007",
+	name: "",
 	price: "4.75",     
-	desc: "taste the citrus",
-	img: "docs/assets/images/slice_ORANGE.jpg"}];
+	desc: "zzzzzzz",
+	img: "docs/assets/images/.jpg"}];
 
 /*let basket = JSON.parse(localStorage.getItem("data")) || [];*/
+
 
 
 
