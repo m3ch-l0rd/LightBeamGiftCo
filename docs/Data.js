84fafd4b45fa1ab48c3cdf01189baa22cadd06b5
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-01-10 00:20:43 -0600

    Update Data.js

--- docs/Data.js
+++ docs/Data.js
@@ -59,17 +59,18 @@ let shopItemsData = [
 	id: "oog",
 	name: "cake_POP",
 	price: "4.75",
-	desc: "hunt those demons down ",
-	img: "docs/assets/images/drip_VINE.jpg"},	
+	desc: "hunt those demons gurl ",
+	img: "docs/assets/images/cake_POP.jpg"},	
 
 	{
 	id: "ooh",
-	name: "llllllll",
-	price: "4.75",     
-	desc: "opopopopop",
-	img: "docs/assets/images/cosmos_FLOWER.jpg"}];
+	name: "heavy_ROCKS",
+	price: "32.75",     
+	desc: "its like drinking out of a giant ice cube",
+	img: "docs/assets/images/libbyGLASS.jpg"}];
 
 /*let basket = JSON.parse(localStorage.getItem("data")) || [];*/
+
 
 
 
