cf31ae7d42adbac13dfdff536a97fc5277d54992
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-01-20 13:07:02 -0600

    Update Data.js

--- docs/Data.js
+++ docs/Data.js
@@ -1,41 +1,41 @@
 let shopItemsData = [
 	{
-	id: "ooa",
+	id: "price_1SoBFZBHlMQxpyuOg0duec41",
 	name: "mode_ZEN",
 	price: "4.75",
 	desc: "sanctuary in the solidtude of self",
 	img: "docs/assets/images/mode_ZEN.jpg"},
 	
 	{
-	id: "oob",
+	id: "price_1SdsRSBHlMQxpyuO2Dd9qCS9",
 	name: "flower_SUN",
 	price: "4.75",
 	desc: "I'm gonna soak up the sun I'm gonna tell everyone",
 	img: "docs/assets/images/starFLOWER.jpg"},
 		
 	{
-	id: "ooc",
+	id: "price_1SoBKvBHlMQxpyuOD7oWfBsx",
 	name: "plumerian_EQUILIBRIUM",
 	price: "4.75",
 	desc: "BEAUTIFLLY balanced.. or imbalanced..",
 	img: "docs/assets/images/plumerian_EQUILIBRIUM.jpg"},	
 
 	{
-	id: "ood",
+	id: "price_1SoBQ2BHlMQxpyuOVQpYVpx0",
 	name: "slice_ORANGE",
 	price: "4.75",     
 	desc: "taste the citrus, be the citrus",
 	img: "docs/assets/images/slice_ORANGE.jpg"},
 	
 	{
-	id: "ooe",
+	id: "price_1SoBZnBHlMQxpyuOESysKblx",
 	name: "bird_LOVE",
 	price: "4.75",
 	desc: "rare peace, selfless care",
 	img: "docs/assets/images/bird_LOVE.jpg"},
 	
 	{
-	id: "oof",
+	id: "price_1SoBV9BHlMQxpyuOIna3AlPh",
 	name: "bud_ROSE",
 	price: "4.75",
 	desc: "I like to think about big phat roses...",
@@ -56,7 +56,7 @@ let shopItemsData = [
 	img: "docs/assets/images/cosmos_FLOWER.jpg"},
 
 	{
-	id: "ooi",
+	id: "price_1SoBVmBHlMQxpyuOQfqhm3N7",
 	name: "cake_POP",
 	price: "4.75",
 	desc: "hunt those demons gurl ",
@@ -70,6 +70,7 @@ let shopItemsData = [
 	img: "docs/assets/images/libbyGLASS.jpg"}];
 
 /*let basket = JSON.parse(localStorage.getItem("data")) || [];*/
+
 
 
 
