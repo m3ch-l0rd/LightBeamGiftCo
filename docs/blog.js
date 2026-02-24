ca3c3f9c7b9303bbafc9d3d9c7a5cd95da56b57b
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-02-23 23:11:21 -0600

    Add files via upload
    add js files and html files for blog feature  update

---
+++ docs/blog.js
@@ -0,0 +1,26 @@
+
+
+let postBoard = document.getElementById("postBoard");
+let postBasket = JSON.parse(localStorage.getItem("data")) || [];
+
+
+let generatePostBoard = () => {
+
+    return (postBoard.innerHTML = posts.map((x) => {
+
+        let {img, date, title, para} = x;
+        let search = postBasket.find((x) => x.date === date) || [];
+
+        return `
+        <div class="post">
+            <img width="440px" src="${img}"></img>
+            <div>
+                <h3>${date}</h3>
+                <h3>${title}</h3>
+                <p>${para}</p>
+            </div>
+        </div>`;
+        }).join(""));
+     };
+
+generatePostBoard();
---
+++ docs/blogPostData.js
@@ -0,0 +1,7 @@
+
+
+let posts = [{
+    img: "assets/images/kampf.jpg",
+    date: "02132026",
+    title: "FIRST BLOG POST EVER!",
+    para: "Well here we are! Growing and on our way to becoming something! Look at us go! We did it! Part of it anyway."}]
