a24614624eb449689c4528cbd0a537d056a1f504
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-02-26 16:33:36 -0600

    Update blogPostData.js

--- docs/blogPostData.js
+++ docs/blogPostData.js
@@ -4,4 +4,9 @@ let posts = [{
     img: "assets/images/kampf.jpg",
     date: "02132026",
     title: "FIRST BLOG POST EVER!",
+
+    para: "Well here we are! Growing and on our way to becoming something! Look at us go! We did it! Part of it anyway."},{
+    img: "assets/images/kampf.jpg",
+    date: "02132026",
+    title: "FIRST BLOG POST EVER!",
     para: "Well here we are! Growing and on our way to becoming something! Look at us go! We did it! Part of it anyway."}]
