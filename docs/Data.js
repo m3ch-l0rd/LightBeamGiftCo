fd73b4fc172ee56c921c6d6728c79bac6501b667
m3ch-l0rd <109575141+m3ch-l0rd@users.noreply.github.com>
2026-03-08 20:52:17 -0500

    Update Data.js

--- docs/Data.js
+++ docs/Data.js
@@ -5,7 +5,7 @@ let shopItemsData = [
 	price: "4.75",
 	desc: "sanctuary in the solidtude of self",
 	img: "docs/assets/images/mode_ZEN.jpg",
-	alt: "#wood earrings"},
+	alt: "natural finish lazer cut wood earrings"},
 	
 	{
 	id: "price_1SdsRSBHlMQxpyuO2Dd9qCS9",
@@ -71,6 +71,7 @@ let shopItemsData = [
 	img: "docs/assets/images/libbyGLASS.jpg"}];
 
 /*let basket = JSON.parse(localStorage.getItem("data")) || [];*/
+
 
 
 
